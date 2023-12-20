65ee1d071e8177b02d623f5c770a6aabf9fd5a39
fcjcbk <1097086201@qq.com>
2023-12-20 22:22:57 +0800

    Fix import statement in main.py and add init-hook in .pylintrc

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
-from .logger import get_logger
+from logger import get_logger
 
 # to get a string like this run:
 # openssl rand -hex 32
